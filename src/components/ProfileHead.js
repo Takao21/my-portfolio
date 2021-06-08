5aa53b3880374210c60dabacb300ef64c4236318
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-06-08 19:29:35 +0630

    minor bootstrap fix

--- src/components/ProfileHead.js
+++ src/components/ProfileHead.js
@@ -11,21 +11,23 @@ export const ProfileHead = ({ apiBasicInfo }) => {
     dispatch(updateData(apiBasicInfo));
   }, []);
   return (
-    <div className="ProfileHead row justify-content-around">
-      <div className="image-container col-2">
-        <img
-          className="profile-image"
-          src="https://i.pinimg.com/originals/bd/2e/23/bd2e2373e77d43f6b48d6fceafd96a2b.jpg"
-          alt="profile"
-        ></img>
-      </div>
-      <div className="headline-container col-8 text-left">
-        <h3>Welcome to my world!</h3>
-        <hr></hr>
-        <h5>Name : {data.name}</h5>
-        <h5>Date of Birth : {data.birthday}</h5>
-        <h5>Gender : {data.gender}</h5>
-        <h5>Occupation : {data.occupation}</h5>
+    <div className="ProfileHead container-fluid">
+      <div className="common-container row justify-content-around">
+        <div className="image-container col-2">
+          <img
+            className="profile-image"
+            src="https://i.pinimg.com/originals/bd/2e/23/bd2e2373e77d43f6b48d6fceafd96a2b.jpg"
+            alt="profile"
+          ></img>
+        </div>
+        <div className="headline-container col-8 text-left">
+          <h3>Welcome to my world!</h3>
+          <hr></hr>
+          <h5>Name : {data.name}</h5>
+          <h5>Date of Birth : {data.birthday}</h5>
+          <h5>Gender : {data.gender}</h5>
+          <h5>Occupation : {data.occupation}</h5>
+        </div>
       </div>
     </div>
   );
