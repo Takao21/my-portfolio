4646f57f7123abc8e5e4e7417275ba3a334ec2b2
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-06-10 19:30:02 +0630

    added biography section on homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ProfileAbout } from "../components/ProfileAbout";
+import { ProfileBio } from "../components/ProfileBio";
 import { ProfileHead } from "../components/ProfileHead";
 import { basicInfo } from "../_json-placeholder/api_getBasicInfo";
 import "./HomePage.css";
@@ -11,6 +12,7 @@ export const HomePage = () => {
     <div className="HomePage">
       <ProfileHead apiBasicInfo={apiBasicInfo} />
       <ProfileAbout about={apiBasicInfo.about} />
+      <ProfileBio />
     </div>
   );
 };
