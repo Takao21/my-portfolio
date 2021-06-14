23773d7028089570af78f594e6fc5a6835028864
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-06-14 18:38:49 +0630

    added more skills

--- src/_fake-api/api_skills.js
+++ src/_fake-api/api_skills.js
@@ -22,7 +22,7 @@ export const skills = [
   {
     id: 4,
     imageUrl:
-      "http://blog.addthiscdn.com/wp-content/uploads/2014/11/addthis-react-flux-javascript-scaling.png",
+      "https://www.itechinsiders.com/wp-content/uploads/2021/04/1_okufk5mMDbTfqA5iS_rldQ.png",
     skillName: "ReactJS",
     skillDetail: "Detail of ReactJS",
   },
@@ -31,5 +31,40 @@ export const skills = [
     imageUrl: "https://miro.medium.com/max/375/1*N7mMmyQd_o_J9mnY05suMw.png",
     skillName: "Functional Programming",
     skillDetail: "Detail of Functional Programming",
+  },
+  {
+    id: 6,
+    imageUrl:
+      "https://icons-for-free.com/iconfiles/png/512/super+tiny+icons+angular-1324450705563212320.png",
+    skillName: "AngularJS",
+    skillDetail: "Detail of AngularJS",
+  },
+  {
+    id: 7,
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Inkscape_logo_2.svg/1024px-Inkscape_logo_2.svg.png",
+    skillName: "Inkscape",
+    skillDetail: "Detail of Inkscape",
+  },
+  {
+    id: 8,
+    imageUrl:
+      "https://coralogix.com/wp-content/uploads/2018/04/Coralogix-Nodejs-integration.jpg",
+    skillName: "NodeJS",
+    skillDetail: "Detail of NodeJS",
+  },
+  {
+    id: 9,
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png",
+    skillName: "TypeScript",
+    skillDetail: "Detail of TypeScript",
+  },
+  {
+    id: 10,
+    imageUrl:
+      "https://pluralsight.imgix.net/paths/path-icons/csharp-e7b8fcd4ce.png",
+    skillName: "C#",
+    skillDetail: "Detail of C#",
   },
 ];
