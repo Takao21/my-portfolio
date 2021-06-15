7eef1780d52a78ec4d3d25c694da3a5e20a2e958
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-06-15 21:04:40 +0630

    resized cards, added quote

--- src/pages/SkillsPage.js
+++ src/pages/SkillsPage.js
@@ -8,9 +8,16 @@ export const SkillsPage = () => {
   const apiSkills = skills;
   return (
     <div className="SkillsPage">
+      <div className="quote-div text-center">
+        <blockquote class="my-quote">
+          Don't get bothered by what you can't control. Focus on what you can
+          do.
+          <footer className="quote-footer">Takao</footer>
+        </blockquote>
+      </div>
       <div className="row">
         {apiSkills.map((skill) => (
-          <div key={skill.id} className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+          <div key={skill.id} className="col-lg-2 col-md-4 col-sm-6 col-xs-12">
             <SkillCard skill={skill} />
           </div>
         ))}
