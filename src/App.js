46747ce11fdc0fd9ebfba0787c4b6703b5b93a99
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-10-18 23:17:00 +0630

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { NavBar } from "./common_components/NavBar";
 import { OverlayIntro } from "./common_components/OverlayIntro";
 import { PageFooter } from "./common_components/PageFooter";
 import { CurrentPage } from "./components/CurrentPage";
-require("dotenv").config();
 
 function App() {
   const [overlayActive, setOverlayActive] = useState(true);
--- src/common_components/NavBar.js
+++ src/common_components/NavBar.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
-require("dotenv").config();
 
 export const NavBar = () => {
   return (
