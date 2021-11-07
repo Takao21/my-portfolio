cfc3586669e23b81ceb66e837d15335dca7c8acb
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-11-07 21:20:06 +0630

    redesigning skills page

--- src/pages/SkillsPage.js
+++ src/pages/SkillsPage.js
@@ -7,6 +7,22 @@ export const SkillsPage = () => {
   //fake api
   const apiSkills = skills;
   return (
+    // <div className="SkillsPage">
+    //   <div className="quote-div text-center">
+    //     <blockquote className="my-quote">
+    //       Don't get bothered by what you can't control. Focus on what you can
+    //       do.
+    //       <footer className="quote-footer">Takao</footer>
+    //     </blockquote>
+    //   </div>
+    //   <div className="card-div row">
+    //     {apiSkills.map((skill) => (
+    //       <div key={skill.id} className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+    //         <SkillCard skill={skill} />
+    //       </div>
+    //     ))}
+    //   </div>
+    // </div>
     <div className="SkillsPage">
       <div className="quote-div text-center">
         <blockquote className="my-quote">
@@ -15,12 +31,61 @@ export const SkillsPage = () => {
           <footer className="quote-footer">Takao</footer>
         </blockquote>
       </div>
-      <div className="card-div row">
-        {apiSkills.map((skill) => (
-          <div key={skill.id} className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <SkillCard skill={skill} />
+      <div className="main-skills-div text-center">
+        <div className="main-skill prl-1">
+          <h2>HTML</h2>
+        </div>
+        <div className="main-skill prl-2">
+          <h2>CSS</h2>
+        </div>
+        <div className="main-skill prl-3">
+          <h2>JS</h2>
+        </div>
+        <div className="main-skill prl-4">
+          <h2>React</h2>
+        </div>
+      </div>
+      <div className="projects-div">
+        <div className="category-title">
+          <h3>HTML Projects</h3>
+        </div>
+        <div className="prj-cards-container d-flex">
+          <div className="prj-card">
+            <img
+              className="prj-img"
+              src="https://lastresortwebdesign.com/wp-content/uploads/2018/01/sample-milos.jpg"
+              alt=""
+            ></img>
           </div>
-        ))}
+          <div className="prj-card">
+            <img
+              className="prj-img"
+              src="https://www.onressoftware.com/wp-content/uploads/2019/10/Screen-Shot-2019-10-15-at-TuesdayOctober1510.00PM.png"
+              alt=""
+            ></img>
+          </div>
+        </div>
+      </div>
+      <div className="projects-div">
+        <div className="category-title">
+          <h3>CSS Projects</h3>
+        </div>
+        <div className="prj-cards-container d-flex">
+          <div className="prj-card">
+            <img
+              className="prj-img"
+              src="https://dynamic.websimages.com/s/themes/refined/v1.25/screenshots/thumb-red.png"
+              alt=""
+            ></img>
+          </div>
+          <div className="prj-card">
+            <img
+              className="prj-img"
+              src="https://dynamic.websimages.com/s/themes/classy/v1.28/screenshots/thumb-paisley.png"
+              alt=""
+            ></img>
+          </div>
+        </div>
       </div>
     </div>
   );
