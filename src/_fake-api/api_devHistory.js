7cf05a9d46af05417e0342e4fc63a2869fce993e
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-11-13 17:55:05 +0630

    changed story to third person

--- src/_fake-api/api_devHistory.js
+++ src/_fake-api/api_devHistory.js
@@ -1,80 +1,86 @@
 export const devHistory = [
   {
+    entry: 0,
+    date: "Childhood",
+    story:
+      "He was one of the top students throughout his Primary to High School life, with some experience competing in local top-student competitions. Ranked in Class A or B for most years among 10 classes.",
+  },
+  {
     entry: 1,
     date: "May, 2017",
     story:
-      'My hobby of watching animes and keen interest in softwares led me to work as "Wordpress Web Editor & Content Creator" on Anidl.org, a video downloading website for animes, with some senior developers from India.',
+      'His hobby of watching animes (Japanese animations) and keen interest in softwares led him to work part-time as "Wordpress Web Editor & Content Creator" on AniDL.org, a video downloading website for animes, with some senior developers from India.',
   },
   {
     entry: 2,
     date: "December, 2017",
     story:
-      "Passed the Matriculation Exam with flying colors and enrolled at Yangon Technological University, which is considered as one of the best in Myanmar. Major in Computer Engineering Information Technology.",
+      "Passed the Matriculation Exam with flying colors, achieving Distinctions in Applied Chemistry, Physics, Biology, Maths and English, and enrolled at Yangon Technological University, which is considered as one of the best in Myanmar. Major in Computer Engineering Information Technology.",
   },
   {
     entry: 3,
     date: "April, 2018",
     story:
-      "Together with my classmates, I took a course on C++ with a private tutor for 10 weeks. That was the first encounter and step into learning programming languages.",
+      "Together with his classmates, he took a course on C++ with a private tutor for 10 weeks. That was the first encounter and step into learning programming languages.",
   },
   {
     entry: 4,
     date: "June, 2018",
     story:
-      "I was fascinated by how video games were created after attending a Global Game Jam. So I studied C# and Unity Engine online in my free time. This experience gave me great knowledge of OOP, UI Concepts and Project Management. I also made a small platforming game which I allowed my best friends to play.",
+      "He was fascinated by how video games are created after attending a Global Game Jam. So he studied C# and Unity Engine online in his free time. This experience gave him great knowledge of OOP, UI Concepts and Project Management. He also made a small game like Super Mario which he allowed his best friends to play.",
   },
   {
     entry: 5,
     date: "September, 2019",
     story:
-      "My desire to work in the real-world IT industry drove me to study web development on my own with any available resources out there. I was awarded the certification in HTML5, CSS3 from FreeCodeCamp.org.",
+      "His interest to work in the real-world IT industry drove him to study web development on his own with the available resources out there. Awarded the Certification in HTML5 & CSS3 from FreeCodeCamp.org.",
   },
   {
     entry: 6,
     date: "November, 2019",
     story:
-      "Further continuing my study on web, I achieved the JavaScript Data Structures & Algorithms Certification from FreeCodeCamp in the following few months.",
+      "Further continuing his study on web technologies, he achieved the JavaScript Data Structures & Algorithms Certification from FreeCodeCamp in the following few months.",
   },
   {
     entry: 7,
     date: "December, 2019",
     story:
-      "I developed a webpage for an extracurriculum student club I managed at the university and got to learn the animation framework Anime.js along the way.",
+      "Developed a landing page for an extracurriculum student club he managed at the university and got to learn the animation framework Anime.js along the way.",
   },
   {
     entry: 8,
     date: "February, 2020",
     story:
-      "To improve my JavaScript skills, I went for a professional course on JavaScript, ReactJS & NodeJS at Turing Programming Language Center by Ko Thet Khine, who has enormous experience in Myanmar developer industry.",
+      "To improve JavaScript skills, he went for a professional course on JavaScript, ReactJS & NodeJS at Turing Programming Language Center by Ko Thet Khine, who has enormous experience in Myanmar developer industry.",
   },
   {
     entry: 9,
     date: "March, 2020",
     story:
-      "With the suggestion of my seniors, I chose to learn Bootstrap CSS framework and SCSS/SASS to expand my front-end skill sets.",
+      "With the suggestion of seniors, he chose to learn Bootstrap CSS framework and SCSS/SASS to expand his front-end skill sets.",
   },
   {
     entry: 10,
     date: "April, 2020",
     story:
-      "COVID-19 Outbreak happened and all our routines became limited to home. I made good use of it by learning the Angular framework with a free course from Mosh Himedani (Udemy) and some other books.",
+      "COVID-19 Outbreak happened and all our routines became limited to home. He made good use of it by learning the Angular framework with a free course from Mosh Himedani (Udemy) and some other books.",
   },
   {
     entry: 11,
     date: "June, 2020",
     story:
-      "I had a months-long online training with seniors and classmates from my university in a private project for the school. Here I got more experience working with an amateur team using ReactJS and learnt about Material UI, Databases and Design Thinking Of A Website.",
+      "Had a months-long online training with seniors and classmates from his university in a private project for the school. Got more experience working in an amateur team using ReactJS and learnt about Material UI, Databases and Design Thinking Of A Website.",
   },
   {
     entry: 12,
     date: "August, 2020",
     story:
-      "Won a start-up challenge by Phandeeyar, a local innovation center, and co-founded Myan Digital - which helps traditional businesses start online sales and improve work quality with technology. I gained a lot of experience in communication, management, digital marketing, UX Thinking, Graphic Design and many more.",
+      "Won a start-up challenge by Phandeeyar, a local innovation center, and co-founded Myan Digital - which helps traditional businesses start online sales and improve work quality with technology. He gained lots of experience in communication, management, digital marketing, UX Thinking, Graphic Design and many more.",
   },
   {
     entry: 13,
     date: "February, 2021",
     story:
-      "For about half a year, I was successfully running my start-up while honing my technical skills. But on Feb 2021, the infuriating Myanmar Coup D' Etat happened. My growing start-up was forced to stop and I had to live in a frozen hell of despair to the present day.",
+      "For about half a year, he was successfully running his start-up while honing his technical skills. But on Feb 2021, the infuriating Myanmar Coup D' Etat happened. The growing start-up was forced to stop and he had to live in a frozen hell of despair to the present day.",
   },
 ];
