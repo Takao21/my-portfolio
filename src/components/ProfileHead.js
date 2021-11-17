c19df25d72c05e8cc1d422937ed2463d8c55a414
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-11-17 17:47:34 +0630

    code clean up

--- src/components/ProfileHead.js
+++ src/components/ProfileHead.js
@@ -4,7 +4,7 @@ import "./ProfileHead.css";
 import "../reducers";
 import { updateData } from "../actions";
 
-export const ProfileHead = ({ apiBasicInfo, apiMediaUrls }) => {
+export const ProfileHead = ({ apiBasicInfo }) => {
   const dispatch = useDispatch();
   const data = useSelector((state) => state.updateDataReducer);
   useEffect(() => {
@@ -12,40 +12,6 @@ export const ProfileHead = ({ apiBasicInfo, apiMediaUrls }) => {
     // eslint-disable-next-line
   }, []);
   return (
-    // <div className="ProfileHead container-fluid">
-    //   <div className="common-container row justify-content-around">
-    //     <div className="image-container col-2">
-    //       {/* Image Side */}
-    //       <img
-    //         className="profile-image"
-    //         src={apiMediaUrls.urlProfileImgSquare}
-    //         alt="profile"
-    //       ></img>
-    //     </div>
-    //     <div className="headline-container col-8 text-left">
-    //       {/* Text Side */}
-    //       <h2 className="welcome-header">{data.name}</h2>
-    //       <hr></hr>
-    //       <h5>
-    //         <span className="info-type">Aliases</span> :{" "}
-    //         {data.aliases.map((alias, index, arr) => (
-    //           <span key={index}>
-    //             {index === arr.length - 1 ? alias : alias + ", "}{" "}
-    //           </span>
-    //         ))}
-    //       </h5>
-    //       <h5>
-    //         <span className="info-type">Date of Birth</span> : {data.birthday}
-    //       </h5>
-    //       <h5>
-    //         <span className="info-type">Gender</span> : {data.gender}
-    //       </h5>
-    //       <h5>
-    //         <span className="info-type">Occupation</span> : {data.occupation}
-    //       </h5>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="ProfileHead">
       <div className="hero-text">
         <p>
