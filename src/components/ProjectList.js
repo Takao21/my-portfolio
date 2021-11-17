b1df6d82334315e45d876385125e765194f39bb0
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-11-17 19:50:11 +0630

    conditionally render projects

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -5,68 +5,34 @@ import "./ProjectList.css";
 
 export const ProjectList = ({ topic, projects }) => {
   return (
-    <div className="ProjectList">
-      <div className="category-title">
-        <h3>{topic} Projects</h3>
-        {console.log(topic, "projects are", projects)}
-      </div>
-      <hr />
-      <div className="prj-cards-container d-flex">
-        {projects.map((prj) => (
-          <div className="prj-card" key={prj.id}>
-            <img
-              className="prj-img"
-              src={prj.imageURL}
-              alt={prj.imageAlt}
-            ></img>
-            <div className="card-desc">
-              <a href={prj.sourceURL} target="_blank" rel="noreferrer">
-                <span className="see-details-btn">
-                  <FontAwesomeIcon icon={faArrowRight} />
-                </span>
-              </a>
-              <p className="card-title">{prj.title}</p>
-              <p>{prj.shortDesc}</p>
+    !!projects.length && (
+      <div className="ProjectList">
+        <div className="category-title">
+          <h3>{topic} Projects</h3>
+          {console.log(topic, "projects are", projects)}
+        </div>
+        <hr />
+        <div className="prj-cards-container d-flex">
+          {projects.map((prj) => (
+            <div className="prj-card" key={prj.id}>
+              <img
+                className="prj-img"
+                src={prj.imageURL}
+                alt={prj.imageAlt}
+              ></img>
+              <div className="card-desc">
+                <a href={prj.sourceURL} target="_blank" rel="noreferrer">
+                  <span className="see-details-btn">
+                    <FontAwesomeIcon icon={faArrowRight} />
+                  </span>
+                </a>
+                <p className="card-title">{prj.title}</p>
+                <p>{prj.shortDesc}</p>
+              </div>
             </div>
-          </div>
-        ))}
-        {/* <div className="prj-card">
-          <img
-            className="prj-img"
-            src="https://lastresortwebdesign.com/wp-content/uploads/2018/01/sample-milos.jpg"
-            alt=""
-          ></img>
-          <div className="card-desc">
-            <span className="see-details-btn">
-              <FontAwesomeIcon icon={faArrowRight} />
-            </span>
-            <p className="card-title">Smol Title</p>
-            <p>
-              A very fancy project made for a very interesting topic to share
-              with very amazing people and do very crazy stuff.
-            </p>
-          </div>
-        </div> */}
-        {/* <div className="prj-card">
-          <img
-            className="prj-img"
-            src="https://34sp.net/img/www/sample-sites/pingcreates.1.jpg"
-            alt=""
-          ></img>
-          <div className="card-desc">
-            <span className="see-details-btn">
-              <FontAwesomeIcon icon={faArrowRight} />
-            </span>
-            <p className="card-title">Project Title</p>
-            <p>
-              A very fancy project made for a very interesting topic to share
-              with very amazing people and do very crazy stuff. Let me extend
-              this one a bit more than other cards' descriptions so that I can
-              test unique cases of exceptionally lengthy texts on card.
-            </p>
-          </div>
-        </div> */}
+          ))}
+        </div>
       </div>
-    </div>
+    )
   );
 };
--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -14,12 +14,33 @@ export const PortfolioPage = () => {
     apiProjects.filter(
       (prj) => prj.categories.includes("html") || prj.categories.includes("css")
     );
+  const jsProjects =
+    apiProjects &&
+    apiProjects.filter(
+      (prj) =>
+        prj.categories.includes("js") || prj.categories.includes("javascript")
+    );
+  const bootstrapProjects =
+    apiProjects &&
+    apiProjects.filter((prj) => prj.categories.includes("bootstrap"));
+  const reactProjects =
+    apiProjects &&
+    apiProjects.filter(
+      (prj) =>
+        prj.categories.includes("react") || prj.categories.includes("redux")
+    );
+  const ngProjects =
+    apiProjects &&
+    apiProjects.filter((prj) => prj.categories.includes("angular"));
 
   return (
     <div className="PortfolioPage">
       <BigQuote />
       <SkillDiamonds />
       <ProjectList topic="HTML, CSS" projects={htmlcssProjects} />
+      <ProjectList topic="JavaScript" projects={jsProjects} />
+      <ProjectList topic="React/Redux" projects={reactProjects} />
+      <ProjectList topic="Angular" projects={ngProjects} />
     </div>
   );
 };
