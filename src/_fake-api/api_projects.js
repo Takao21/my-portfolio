abca1b792ddf23c79dfb3b46d9cca22b8dee7cc4
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-11-17 19:24:43 +0630

    component refactoring

---
+++ src/_fake-api/api_projects.js
@@ -0,0 +1,33 @@
+export const projects = [
+  {
+    id: 1,
+    categories: ["html", "css"],
+    title: "Project Title Is Very Long Indeed",
+    imageURL:
+      "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+    imageAlt: "image alt",
+    shortDesc:
+      "This is one of my proudest projects in my early days of learning web. My father asked me about the Voucher printing apps going around in the local businesses. So, instead of buying those softwares, I wanted to try making one myself since I'm a proud programmer. Thus, using my knowledge of web programming, I created this Voucher Printing Webapp for my dad's shop. It's not an amazing project since it depends on Chrome to run but it will show my dedication and competitive mindset in technology. I felt good being able to help my dad using the technologies I've learnt.",
+    sourceURL: "https://www.google.com",
+  },
+  {
+    id: 2,
+    categories: ["html", "css"],
+    title: "project title 2",
+    imageURL:
+      "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+    imageAlt: "image alt 2",
+    shortDesc: "lorem ipsum 2",
+    sourceURL: "https://www.google.com",
+  },
+  {
+    id: 3,
+    categories: ["react"],
+    title: "project title React",
+    imageURL:
+      "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png",
+    imageAlt: "image alt react",
+    shortDesc: "lorem ipsum react",
+    sourceURL: "https://www.google.com",
+  },
+];
--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -1,101 +1,25 @@
 import React from "react";
 import "./PortfolioPage.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { BigQuote } from "../components/BigQuote";
 import { SkillDiamonds } from "../components/SkillDiamonds";
+import { projects } from "../_fake-api/api_projects";
+import { ProjectList } from "../components/ProjectList";
 
 export const PortfolioPage = () => {
+  // fake api
+  console.log("API Projects are ", projects);
+  const apiProjects = projects;
+  const htmlcssProjects =
+    apiProjects &&
+    apiProjects.filter(
+      (prj) => prj.categories.includes("html") || prj.categories.includes("css")
+    );
+
   return (
     <div className="PortfolioPage">
       <BigQuote />
       <SkillDiamonds />
-      <div className="projects-div">
-        <div className="category-title">
-          <h3>Personal Projects</h3>
-        </div>
-        <hr />
-        <div className="prj-cards-container d-flex">
-          <div className="prj-card">
-            <img
-              className="prj-img"
-              src="https://lastresortwebdesign.com/wp-content/uploads/2018/01/sample-milos.jpg"
-              alt=""
-            ></img>
-            <div className="card-desc">
-              <span className="see-details-btn">
-                <FontAwesomeIcon icon={faArrowRight} />
-              </span>
-              <p className="card-title">Smol Title</p>
-              <p>
-                A very fancy project made for a very interesting topic to share
-                with very amazing people and do very crazy stuff.
-              </p>
-            </div>
-          </div>
-          <div className="prj-card">
-            <img
-              className="prj-img"
-              src="https://34sp.net/img/www/sample-sites/pingcreates.1.jpg"
-              alt=""
-            ></img>
-            <div className="card-desc">
-              <span className="see-details-btn">
-                <FontAwesomeIcon icon={faArrowRight} />
-              </span>
-              <p className="card-title">Project Title</p>
-              <p>
-                A very fancy project made for a very interesting topic to share
-                with very amazing people and do very crazy stuff. Let me extend
-                this one a bit more than other cards' descriptions so that I can
-                test unique cases of exceptionally lengthy texts on card.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="projects-div">
-        <div className="category-title">
-          <h3>Collaboration Projects</h3>
-        </div>
-        <hr />
-        <div className="prj-cards-container d-flex">
-          <div className="prj-card">
-            <img
-              className="prj-img"
-              src="https://dynamic.websimages.com/s/themes/refined/v1.25/screenshots/thumb-red.png"
-              alt=""
-            ></img>
-            <div className="card-desc">
-              <span className="see-details-btn">
-                <FontAwesomeIcon icon={faArrowRight} />
-              </span>
-              <p className="card-title">Project Title</p>
-              <p>
-                A very fancy project made for a very interesting topic to share
-                with very amazing people and do very crazy stuff.
-              </p>
-            </div>
-          </div>
-          <div className="prj-card">
-            <img
-              className="prj-img"
-              src="https://dynamic.websimages.com/s/themes/classy/v1.28/screenshots/thumb-paisley.png"
-              alt=""
-            ></img>
-            <div className="card-desc">
-              <span className="see-details-btn">
-                <FontAwesomeIcon icon={faArrowRight} />
-              </span>
-              <p className="card-title">A Pretty Long Project Title To Test</p>
-              <p>
-                A very fancy project made for a very interesting topic to share
-                with very amazing people and do very crazy stuff.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <ProjectList topic="HTML, CSS" projects={htmlcssProjects} />
     </div>
   );
 };
