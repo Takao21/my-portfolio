27f0e06cd393a413db482adedc63a4926abecac7
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-11-17 17:39:24 +0630

    deleted profile about components

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,17 +3,15 @@ import { ProfileBody } from "../components/ProfileBody";
 import { ProfileHead } from "../components/ProfileHead";
 import { devHistory } from "../_fake-api/api_devHistory";
 import { basicInfo } from "../_fake-api/api_getBasicInfo";
-import { mediaUrls } from "../_fake-api/api_media";
 import "./HomePage.css";
 
 export const HomePage = () => {
   // placeholder API
   const apiBasicInfo = basicInfo;
-  const apiMediaUrls = mediaUrls;
   const apiDevHistory = devHistory;
   return (
     <div className="HomePage">
-      <ProfileHead apiBasicInfo={apiBasicInfo} apiMediaUrls={apiMediaUrls} />
+      <ProfileHead apiBasicInfo={apiBasicInfo} />
       <ProfileBody apiDevHistory={apiDevHistory} />
     </div>
   );
--- src/pages/SkillsPage.js
+++ src/pages/SkillsPage.js
@@ -5,25 +5,7 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { skills } from "../_fake-api/api_skills";
 
 export const SkillsPage = () => {
-  //fake api
-  const apiSkills = skills;
   return (
-    // <div className="SkillsPage">
-    //   <div className="quote-div text-center">
-    //     <blockquote className="my-quote">
-    //       Don't get bothered by what you can't control. Focus on what you can
-    //       do.
-    //       <footer className="quote-footer">Takao</footer>
-    //     </blockquote>
-    //   </div>
-    //   <div className="card-div row">
-    //     {apiSkills.map((skill) => (
-    //       <div key={skill.id} className="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-    //         <SkillCard skill={skill} />
-    //       </div>
-    //     ))}
-    //   </div>
-    // </div>
     <div className="SkillsPage">
       <div className="quote-div text-center">
         <blockquote className="my-quote">
